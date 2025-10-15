b818a61f17d35c8cb1be573f025ef75f0eeff32b
Marco <marco.mangan@gmail.com>
2025-10-15 21:22:14 +0000

    Closes #4

--- App.java
+++ App.java
@@ -1,10 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Scanner;
+
 
 
 public class App {
--- TextAnalysis.java
+++ TextAnalysis.java
@@ -4,7 +4,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashSet;
 
 public class TextAnalysis {
 	
